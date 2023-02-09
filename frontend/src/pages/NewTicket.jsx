51e0e6a3717d8872b99da85582be1305822ea866
Cat Whitmer <60889037+catwhitmer@users.noreply.github.com>
2023-02-09 12:38:36 -0500

    fixed a typo

--- frontend/src/pages/NewTicket.jsx
+++ frontend/src/pages/NewTicket.jsx
@@ -12,7 +12,7 @@ function NewTicket() {
 
   const { user } = useSelector((state) => state.auth);
   const { isLoading, isError, isSuccess, message } = useSelector(
-    (state) => state.ticket
+    (state) => state.tickets
   );
 
   const [name] = useState(user.name);
