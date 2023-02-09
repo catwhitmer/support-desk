6286843fd0fce44c51f5863d233ca98a11b81877
Cat Whitmer <60889037+catwhitmer@users.noreply.github.com>
2023-02-09 12:57:45 -0500

    created a single ticket item

--- frontend/src/pages/Tickets.jsx
+++ frontend/src/pages/Tickets.jsx
@@ -1,10 +1,9 @@
-import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getTickets, reset } from "../features/tickets/ticketSlice";
 import Spinner from "../components/Spinner";
 import BackButton from "../components/BackButton";
-import { toast } from "react-toastify";
+import TicketItem from "../components/TicketItem";
 
 function Tickets() {
   const dispatch = useDispatch();
@@ -30,9 +29,21 @@ function Tickets() {
   }
 
   return (
-    <div>
+    <>
+      <BackButton />
       <h1>Tickets</h1>
-    </div>
+      <div className="tickets">
+        <div className="ticket-headings">
+          <div>Date</div>
+          <div>Product</div>
+          <div>Status</div>
+          <div></div>
+        </div>
+        {tickets.map((ticket) => (
+          <TicketItem key={ticket._id} ticket={ticket} />
+        ))}
+      </div>
+    </>
   );
 }
 
