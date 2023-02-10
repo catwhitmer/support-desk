3602a626ab5a8fef6591d57484a62d981b3ab3a0
Cat Whitmer <60889037+catwhitmer@users.noreply.github.com>
2023-02-10 10:30:03 -0500

    redeploy

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const colors = require("colors");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
+const cors = require("cors");
 const PORT = process.env.PORT || 8000;
 
 // Connect to DB
@@ -13,6 +14,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 // Routes
 app.use("/api/users", require("./routes/userRoutes"));
