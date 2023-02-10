8bb91b3af61a9391055f8e37dc7657f3ac84337b
Cat Whitmer <60889037+catwhitmer@users.noreply.github.com>
2023-02-10 10:01:03 -0500

    bug fix

--- backend/server.js
+++ backend/server.js
@@ -14,10 +14,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Welcome to the Support Desp API" });
-});
-
 // Routes
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/tickets", require("./routes/ticketRoutes"));
@@ -27,11 +23,11 @@ if (process.env.NODE_ENV === "production") {
   // Set build folder as static
   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(__dirname, "../", "frontend", "build", "index.html")
+  app.get("*", (_, res) =>
+    res.sendFile(path.join(__dirname, "../frontend/build/index.html"))
   );
 } else {
-  app.get("/", (req, res) => {
+  app.get("/", (_, res) => {
     res.status(200).json({ message: "Welcome to the Support Desp API" });
   });
 }
